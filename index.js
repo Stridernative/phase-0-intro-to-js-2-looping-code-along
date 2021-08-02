00269d262c4e8d970312a5b0577cbdc73b24615e
gabriel <gabecreacs@gmail.com>
2021-08-02 16:34:49 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+    function writeCards(name){
+        let array=[]
+        for(let i=0;i<name.length;i++){
+            let sentence=`Thank you, ${name[i]}, for the wonderful surprise gift!`
+            array.push(sentence)
+        }
+        return array
+    }
+
+    function countDown(number){
+        let i = number
+        while (i >= 0){
+            console.log(i--)
+        }
+    }
